ed28096f6941879bccfb18edb3071175d224be5e
SamueleSandrini <s.sandrini014@studenti.unibs.it>
2022-05-13 14:16:28 +0200

    bug fix

--- src/pick_place.py
+++ src/pick_place.py
@@ -1,20 +1,17 @@
 #! /usr/bin/env python3
 
 import rospy
-
-# from std_srvs.srv import SetBool,SetBoolResponse
-# from std_msgs.msg import String
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Pose, Point, Quaternion, PoseArray, Transform, Vector3, TransformStamped
 from Gripper import Gripper
-import sys 
+import sys
 import intera_interface              # intera_interface - Sawyer Python API
 import yaml
 import os
 
-import moveit_commander
-import moveit_msgs.msg
-from moveit_commander.conversions import pose_to_list
+# import moveit_commander
+# import moveit_msgs.msg
+# from moveit_commander.conversions import pose_to_list
 
 GREEN = '\033[92m'
 YELLOW = '\033[93m'
@@ -31,20 +28,27 @@ COMMAND_TO_GRIPPER = {"open":"o", "close":"c", "reset":"r","activate":"a"}
 def main():
     rospy.init_node('pick_plance_node', anonymous=True)
     rospy.loginfo(GREEN + "Let's start..." + END)
-    
+
     try:
         object_distribution = rospy.get_param(OBJECT_PARAM)
-    except KeyError:        
+    except KeyError:
         rospy.logerr(PARAM_NOT_DEFINED_ERROR.format(OBJECT_PARAM))
-        return 0    
-    
+        return 0
+
+    try:
+        nominal_speed_ratio = rospy.get_param("nominal_speed_ratio")
+    except KeyError:
+        nominal_speed_ratio = 0.5
+        rospy.logerr(RED + "nominal_speed_ratio not set, set to 0.5" + END)
+        return 0
+
     limb = intera_interface.Limb('right')   # create an instance of intera_interface's Limb class
-    
+
     # Reset Gripper
     gripper = Gripper()
     gripper.genCommand(COMMAND_TO_GRIPPER["reset"])
     gripper.genCommand(COMMAND_TO_GRIPPER["activate"]) # da decommentare se non funziona
-    
+
     ############## Move it
     # moveit_commander.roscpp_initialize(sys.argv)
     # robot = moveit_commander.RobotCommander()
@@ -52,35 +56,37 @@ def main():
     # group_name = "right_arm"
     # group = moveit_commander.MoveGroupCommander(group_name)
     ##############
-    
+
     # Initial set-up robot
     rospy.loginfo("Moving to robot-neutral position with low speed...")
     limb.set_joint_position_speed(speed=0.1)
     limb.move_to_neutral()
-    limb.set_joint_position_speed(speed=1)
+    limb.set_joint_position_speed(speed=nominal_speed_ratio)
     rospy.loginfo("Robot speed set to maximum velocity...")
-    
+
+    t_start = rospy.Time.now()
     for object in object_distribution:
         for position_to_reach in DEFINED_SEQUENCE:
             if position_to_reach in object:
                 set_point_position = object[position_to_reach]
                 limb.move_to_joint_positions(set_point_position)
-                
+
+
+                # With moveit
                 # pose_goal = limb.joint_angles_to_cartesian_pose(set_point_position,end_point='right_hand')
                 # group.set_pose_target(pose_goal)
                 # plan = group.go(wait=True)
                 # # Calling `stop()` ensures that there is no residual movement
                 # group.stop()
-                # # It is always good to clear your targets after planning with poses.
-                # # Note: there is no equivalent function for clear_joint_value_targets()
                 # group.clear_pose_targets()
                 # print(cartesian_pose)
                 # print(type(cartesian_pose))
             if position_to_reach in DEFINED_ROBOTIQ_COMMAND:
-                #aziona la pinza con il comando DEFINED_ROBOTIQ_COMMAND[position_to_reach]
+                # Send command to the gripper
                 gripper.genCommand(COMMAND_TO_GRIPPER[DEFINED_ROBOTIQ_COMMAND[position_to_reach]])
-            rospy.sleep(2)    
+            rospy.sleep(2)      #P Pase
+    t_end = (rospy.Time.now() - t_start).to_sec()
+    rospy.loginfo(GREEN + "Execution time: {} seconds".format(t_end) +END)
 
-    
 if __name__ == "__main__":
     main()
--- src/velocityScaling.py
+++ src/velocityScaling.py
@@ -10,7 +10,7 @@ from std_msgs.msg import Float64
 import tf
 import moveit_commander
 import moveit_msgs.msg
-import sys 
+import sys
 import numpy as np
 
 SKELETON_FILTERED = '/skeleton_filtered'
@@ -20,7 +20,7 @@ RED = '\033[91m'
 BOLD = '\033[1m'
 END = '\033[0m'
 
-HEAD_KEYPOINTS = [0,1,2,3,4,5,6,7,8,9] 
+HEAD_KEYPOINTS = [0,1,2,3,4,5,6,7,8,9]
 LEFT_ARM_KEYPOINTS = [11, 13, 15]
 RIGHT_ARM_KEYPOINTS = [12, 14, 16]
 RIGHT_HAND_KEYPOINTS = [16,18,20,22]
@@ -30,20 +30,27 @@ def callback():
     pass
 
 def main():
-    
-    
+
+
     rospy.init_node('velocity_scaling', anonymous=True)
-    
+
+    try:
+        nominal_speed_ratio = rospy.get_param("nominal_speed_ratio")
+    except KeyError:
+        nominal_speed_ratio = 0.5
+        rospy.logerr(RED + "nominal_speed_ratio not set, set to 0.5" + END)
+        return 0
+
     pub_speed_ratio = rospy.Publisher(
         '/robot/limb/right/set_speed_ratio',
         Float64,
         latch=True,
         queue_size=10)
-    
+
     rate = rospy.Rate(50) # frequency of tf reader
 
-    listener = tf.TransformListener()   
-    
+    listener = tf.TransformListener()
+
     ########################### all this part only to recive all robot-sawyer link, can be replaced by ["right...","righ"]
     # with all robot frame that you want to check distance from human
     moveit_commander.roscpp_initialize(sys.argv)
@@ -51,33 +58,32 @@ def main():
     scene = moveit_commander.PlanningSceneInterface()
     group_name = "right_arm"
     move_group = moveit_commander.MoveGroupCommander(group_name)
-    
+
     # robot_link_to_check = []
     # robot_link_to_check.append(move_group.get_end_effector_link())
     # robot_link_to_check = robot_link_to_check.extend(move_group.get_joints())
-    
+
     robot_link_to_check = move_group.get_joints()
     robot_link_to_check.remove("right_arm_mount")
     print(robot_link_to_check)
-    input("asp")
     robot_tf_link_to_check = ["reference/"+str(single_link) for single_link in robot_link_to_check]
     robot_tf_link_to_check = [single_link.replace("right_j","right_l") for single_link in robot_tf_link_to_check]
     robot_tf_link_to_check = ["reference/right_l6"]
     print(robot_tf_link_to_check)
 
     ################################
-    
-    
-    human_keypoints = [16]              # list of human keypoint to check distance from robot 
-    
+
+
+    human_keypoints = [16]              # list of human keypoint to check distance from robot
+
     # Scaling parameters
     scaling_time = 0.2                  # time required to scale the speed
-    scaling_pub_freq = 100              # scaling pub frequency 
+    scaling_pub_freq = 100              # scaling pub frequency
     rate_scaling_factor = rospy.Rate(scaling_pub_freq)   # rate of sleep of pub. scaling
-    dt = 1/scaling_pub_freq                             
-     
-    actual_speed_ratio = 1 # da leggere da qualche parte
-    
+    dt = 1/scaling_pub_freq
+
+    actual_speed_ratio = nominal_speed_ratio # da leggere da qualche parte
+
     while not rospy.is_shutdown():
         rospy.loginfo("New reading...")
 
@@ -91,34 +97,34 @@ def main():
                     # print(type(trans))
                     #(trans_base_human,rot_base_human) = listener.lookupTransform( "id_"+str(human_keypoint), "base", rospy.Time())
                     (trans_base_human,rot_base_human) = listener.lookupTransform( "base" , "id_"+str(human_keypoint), rospy.Time())
-                    
+
                     (trans_robot_base,rot_robot_base) =  listener.lookupTransform( "base", tf_name_robot_link, rospy.Time())
                     trans_base_human_mat = tf.transformations.translation_matrix(trans_base_human)
                     # rot_base_human_mat = tf.transformations.quaternion_matrix(rot_base_human)
                     trans_robot_base_mat = tf.transformations.translation_matrix(trans_robot_base)
                     # rot_robot_base_mat = tf.transformations.quaternion_matrix(rot_robot_base)
-                    
-                    print(trans_base_human_mat)
-                    
-                    print(trans_robot_base_mat)
-                    
+
+                    # print(trans_base_human_mat)
+                    #
+                    # print(trans_robot_base_mat)
+
                     # M_base_human = np.dot(trans_base_human_mat,rot_base_human_mat)
                     # M_robot_base = np.dot(trans_robot_base_mat,rot_robot_base_mat)
-                    
+
                     # M_robot_human = np.dot(M_robot_base,M_base_human)
                     # # print(M_robot_human)
-                    
+
                     # trans_robot_human = tf.transformations.translation_from_matrix(M_robot_human)
                     # print(type(trans_robot_human))
                     # print(trans_robot_human)
                     #relative_distance = np.linalg.norm(trans_robot_human)
                     relative_distance = np.linalg.norm(trans_robot_base_mat[0:3,3] - trans_base_human_mat[0:3,3])
-                    
+
                     relative_distance -= 0.1
-                    
+
                     distances.append(relative_distance)
-                    
-                    print(relative_distance)
+
+                    rospy.loginfo("Relative distance: {}".format(relative_distance))
                     # if(relative_distance<0.5):
                     #     pub_speed_ratio.publish(0.5)
                     #trans = tfBuffer.lookup_transform("camera_color_optical_frame", "id_"+str(human_keypoint), rospy.Time())
@@ -129,14 +135,14 @@ def main():
                     #     rospy.loginfo("Human keyp " + str(human_keypoint)+ "robot link" + tf_name_robot_link)
                     #     rospy.loginfo(trans_robot_base)
                         pass
-                        
+
                     else:
-                        rospy.loginfo("Troppo vecchia")
+                        rospy.loginfo("Too old...")
                         break
                 except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-                    print("Non  ce")
+                    print("There isn't")
                     #break
-                    #rate.sleep()     
+                    #rate.sleep()
         time_now = (rospy.Time.now() - time).to_sec()
         print(time_now)
         if not distances:
@@ -144,33 +150,33 @@ def main():
             continue
         min_distance = min(distances)
         if min_distance>=1:
-            set_point = 1
+            set_point = nominal_speed_ratio
         elif min_distance>=0.7 and min_distance<1:
-            set_point = 0.6
+            set_point = 0.2
         elif min_distance>=0.3 and min_distance<0.7:
-            set_point = 0.3
+            set_point = 0.1
         elif min_distance<0.3:
             set_point = 0.0
-        
+
         time = rospy.Time.now()
-        
+
         t = 0
         while t < scaling_time:
             actual_speed_ratio = (set_point-actual_speed_ratio)/(scaling_time) * t + actual_speed_ratio
             t += dt
             pub_speed_ratio.publish(actual_speed_ratio)
             rate_scaling_factor.sleep()
-        
+
         time_now = (rospy.Time.now() - time).to_sec()
 
         actual_speed_ratio = set_point
         pub_speed_ratio.publish(actual_speed_ratio)
-        
+
         print(time_now)
         rate.sleep()
     #rospy.Subscriber("/skeleton_filtered", Marker, callback)
-    
-    
-    
+
+
+
 if __name__ == "__main__":
     main()
